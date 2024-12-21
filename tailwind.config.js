859366b7e089ec23ffd6786cf31b16e056eb2721
Yudann <yudannnnn80@gmail.com>
2024-12-21 18:19:33 +0700

    fix: update tailwind config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#161622',
+        primary: '#DAEEEB', // Background utama
         secondary: {
-          DEFAULT: '#FF9C01',
-          100: '#FF9001',
-          200: '#FF8E01',
+          DEFAULT: '#1F7A8C', // Warna utama untuk button
+          accent: '#F4D35E', // Warna accent untuk tombol mencolok
+          highlight: '#F2545B',
         },
         black: {
           DEFAULT: '#000',
-          100: '#1E1E2D',
-          200: '#232533',
+          100: '#1E1E2D', // Untuk teks utama dengan kontras tinggi
+          200: '#084C61', // Warna gelap yang harmonis untuk teks tambahan
         },
         gray: {
-          100: '#CDCDE0',
+          100: '#CDCDE0', // Warna abu terang untuk border atau elemen pendukung
+          200: '#B0D6CE', // Warna hover atau bayangan
+        },
+        shadow: {
+          DEFAULT: 'rgba(0, 0, 0, 0.2)', // Untuk efek bayangan
         },
       },
       fontFamily: {
