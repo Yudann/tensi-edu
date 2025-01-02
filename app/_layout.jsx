b076411c43317247981f37051614ec591d9b9204
Yudann <yudannnnn80@gmail.com>
2025-01-02 22:16:43 +0700

    feat: add blood pressure context on root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,23 +2,15 @@ import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import 'react-native-url-polyfill/auto';
 import { SplashScreen, Stack } from 'expo-router';
-
 import GlobalProvider from '../context/GlobalProvider';
+import BloodPressureProvider from '../context/BloodPressureContext'; // Import provider
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
     'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
     'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
-    // "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    // "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    // "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    // "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    // "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    // "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    // "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
 
   useEffect(() => {
@@ -33,18 +25,20 @@ const RootLayout = () => {
     return null;
   }
 
-  if (!fontsLoaded && !error) {
-    return null;
-  }
-
   return (
     <GlobalProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
-      </Stack>
+      <BloodPressureProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(screen)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="search/[query]"
+            options={{ headerShown: false }}
+          />
+        </Stack>
+      </BloodPressureProvider>
     </GlobalProvider>
   );
 };
