91778b980b2b6ea47834889b895ad5ab609f0fc7
Yudann <yudannnnn80@gmail.com>
2025-01-02 22:13:33 +0700

    feat: add community page

---
+++ app/(screen)/community/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+const Community = () => {
+  return (
+    <View className="flex flex-1 p-4 justify-start items-center bg-primary gap-4">
+      <Text className="text-3xl text-black font-bold">Community</Text>
+    </View>
+  );
+};
+export default Community;
