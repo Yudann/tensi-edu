7b9da6c8694126d0ba0aed19b98faf1e071629d7
Yudann <yudannnnn80@gmail.com>
2025-01-06 21:40:47 +0700

    feat: add recomendations

--- app/(screen)/BloodPressure/index.jsx
+++ app/(screen)/BloodPressure/index.jsx
@@ -1,42 +1,147 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import { useBloodPressure } from '../../../context/BloodPressureContext';
 import { router } from 'expo-router';
 import { FaSave } from 'react-icons/fa';
 
 const BloodPressure = () => {
-  const { bloodPressure, setBloodPressure } = useBloodPressure(); // Mendapatkan context
+  const { bloodPressure, setBloodPressure } = useBloodPressure();
   const [systolic, setSystolic] = useState(bloodPressure.systolic.toString());
   const [diastolic, setDiastolic] = useState(
     bloodPressure.diastolic.toString(),
   );
   const [status, setStatus] = useState('');
   const [color, setColor] = useState('');
+  const [recommendations, setRecommendations] = useState({});
 
-  // Fungsi untuk menentukan status dan warna berdasarkan tekanan darah
+  const recommendationData = {
+    Normal: {
+      color: 'green',
+      tips: [
+        'Makan makanan sehat seperti sayur-sayuran dan buah-buahan setiap hari.',
+        'Kurangi makanan yang terlalu asin atau berminyak.',
+        'Lakukan olahraga ringan, seperti jalan kaki 30 menit sehari, lima kali seminggu.',
+        'Istirahat yang cukup dengan tidur 7-8 jam setiap malam.',
+        'Hindari stres. Luangkan waktu untuk melakukan hal yang menyenangkan atau meditasi.',
+        'Lakukan pemeriksaan tekanan darah secara rutin.',
+      ],
+      foods: [
+        { type: 'Sayur', items: ['Brokoli', 'Bayam', 'Wortel'] },
+        { type: 'Buah', items: ['Apel', 'Jeruk', 'Kiwi'] },
+        {
+          type: 'Protein',
+          items: ['Ikan salmon', 'Ayam tanpa kulit', 'Kacang-kacangan'],
+        },
+      ],
+      complications: [
+        'Risiko terkena hipertensi dan penyakit jantung di masa depan.',
+      ],
+    },
+    Prahipertensi: {
+      color: 'yellow',
+      tips: [
+        'Kurangi makanan yang asin, seperti keripik atau makanan kaleng.',
+        'Batasi penggunaan garam dalam masakan.',
+        'Mulailah makan makanan sehat seperti buah, sayur, dan biji-bijian.',
+        'Lakukan olahraga seperti bersepeda, berenang, atau senam selama 30 menit, minimal 3 kali seminggu.',
+        'Jangan merokok dan hindari alkohol.',
+        'Catat tekanan darah Anda secara berkala untuk melihat apakah ada perubahan.',
+      ],
+      foods: [
+        { type: 'Sayur', items: ['Timun', 'Seledri', 'Asparagus'] },
+        { type: 'Buah', items: ['Pisang', 'Alpukat', 'Stroberi'] },
+        { type: 'Protein', items: ['Ikan tuna', 'Tahu', 'Tempe'] },
+      ],
+      complications: ['Risiko naik menjadi hipertensi grade 1.'],
+    },
+    'Hipertensi Grade 1': {
+      color: 'red',
+      tips: [
+        'Perbanyak makan sayur hijau dan buah-buahan segar setiap hari.',
+        'Kurangi makanan berminyak, seperti gorengan, dan gantikan dengan makanan yang dipanggang atau direbus.',
+        'Lakukan olahraga rutin, seperti jalan cepat atau jogging ringan.',
+        'Batasi konsumsi alkohol dan hentikan kebiasaan merokok.',
+        'Konsultasikan tekanan darah Anda ke dokter untuk pengobatan lebih lanjut.',
+      ],
+      foods: [
+        { type: 'Sayur', items: ['Bayam', 'Kale', 'Brokoli'] },
+        { type: 'Buah', items: ['Anggur', 'Delima', 'Blueberry'] },
+        { type: 'Protein', items: ['Ikan sarden', 'Telur rebus', 'Dada ayam'] },
+      ],
+      complications: [
+        'Risiko terkena penyakit jantung, stroke, atau gagal ginjal.',
+      ],
+    },
+    'Hipertensi Grade 2': {
+      color: 'orange',
+      tips: [
+        'Segera konsultasikan dengan dokter untuk pengobatan lebih lanjut.',
+        'Ikuti pola makan rendah garam dan tinggi serat.',
+        'Hindari stres dan lakukan teknik relaksasi seperti yoga atau meditasi.',
+        'Minum obat antihipertensi sesuai resep dokter.',
+        'Kurangi konsumsi makanan olahan dan pilih makanan alami.',
+      ],
+      foods: [
+        { type: 'Sayur', items: ['Selada', 'Kol', 'Bayam'] },
+        { type: 'Buah', items: ['Semangka', 'Pisang', 'Kiwi'] },
+        { type: 'Protein', items: ['Ikan makarel', 'Kacang almond', 'Tahu'] },
+      ],
+      complications: ['Risiko tinggi terkena serangan jantung atau stroke.'],
+    },
+    'Hipertensi Akut': {
+      color: 'darkred',
+      tips: [
+        'Segera cari bantuan medis darurat.',
+        'Jangan menunda pengobatan untuk menghindari komplikasi yang fatal.',
+        'Tetap tenang dan hindari aktivitas fisik yang berat.',
+      ],
+      foods: [],
+      complications: [
+        'Risiko kerusakan organ akut seperti otak, jantung, atau ginjal.',
+      ],
+    },
+  };
+
   useEffect(() => {
     const systolicValue = parseInt(systolic);
     const diastolicValue = parseInt(diastolic);
 
     if (systolicValue <= 120 && diastolicValue <= 80) {
       setStatus('Normal');
-      setColor('green');
+      setColor(recommendationData.Normal.color);
+      setRecommendations(recommendationData.Normal);
     } else if (
       systolicValue >= 120 &&
       systolicValue < 130 &&
       diastolicValue < 80
     ) {
       setStatus('Prahipertensi');
-      setColor('yellow');
+      setColor(recommendationData.Prahipertensi.color);
+      setRecommendations(recommendationData.Prahipertensi);
     } else if (
       (systolicValue >= 130 && systolicValue < 140) ||
       (diastolicValue >= 80 && diastolicValue < 90)
     ) {
-      setStatus('Hipertensi 1');
-      setColor('red');
-    } else if (systolicValue >= 140 || diastolicValue >= 90) {
-      setStatus('Hipertensi 2');
-      setColor('pink');
+      setStatus('Hipertensi Grade 1');
+      setColor(recommendationData['Hipertensi Grade 1'].color);
+      setRecommendations(recommendationData['Hipertensi Grade 1']);
+    } else if (
+      (systolicValue >= 140 && systolicValue < 180) ||
+      (diastolicValue >= 90 && diastolicValue < 120)
+    ) {
+      setStatus('Hipertensi Grade 2');
+      setColor(recommendationData['Hipertensi Grade 2'].color);
+      setRecommendations(recommendationData['Hipertensi Grade 2']);
+    } else if (systolicValue >= 180 || diastolicValue >= 120) {
+      setStatus('Hipertensi Akut');
+      setColor(recommendationData['Hipertensi Akut'].color);
+      setRecommendations(recommendationData['Hipertensi Akut']);
     }
   }, [systolic, diastolic]);
 
@@ -46,120 +151,106 @@ const BloodPressure = () => {
       diastolic: parseInt(diastolic),
     });
     alert('Blood pressure updated!');
-    router.push('/home');
+    router.push('/Home');
   };
 
   return (
-    <View className="flex-1 justify-center items-center bg-cyan-50 px-6">
-      {/* Judul */}
-      <Text className="text-lg font-bold text-gray-700 mb-4">
-        Catatan Terbaru
-      </Text>
-      <View className={`bg-sky-200 rounded-xl p-6 items-center mb-4 w-full`}>
-        <View
-          className={`w-28 h-28 rounded-full justify-center items-center border-8 ${
-            color === 'green'
-              ? 'border-green-600'
-              : color === 'yellow'
-                ? 'border-yellow-600'
-                : color === 'red'
-                  ? 'border-red-600'
-                  : color === 'pink'
-                    ? 'border-pink-600'
-                    : 'border-blue-500'
-          }`}
-        >
-          <Text
-            className={`text-2xl font-bold ${color === 'green' ? 'text-green-600' : ''} 
-                               ${color === 'yellow' ? 'text-yellow-600' : ''} 
-                               ${color === 'red' ? 'text-red-600' : ''} 
-                               ${color === 'pink' ? 'text-pink-600' : ''}`}
+    <ScrollView className="flex-1 bg-primary p-6">
+      <View className="justify-center items-center">
+        <Text className="text-lg font-bold text-gray-700 mb-4">
+          Catatan Terbaru
+        </Text>
+        <View className={`bg-sky-200 rounded-xl p-6 items-center mb-4 w-full`}>
+          <View
+            className={`w-28 h-28 rounded-full justify-center items-center border-8 ${
+              color === 'green'
+                ? 'border-green-600'
+                : color === 'yellow'
+                  ? 'border-yellow-600'
+                  : 'border-red-600'
+            }`}
           >
-            {bloodPressure.systolic}/{bloodPressure.diastolic}{' '}
-            {/* Display blood pressure */}
-          </Text>
-        </View>
-        <View className="flex-row items-center mt-4">
-          {/* Menampilkan status berdasarkan logika */}
-          <Text
-            className={`text-lg font-bold ${color === 'green' ? 'text-green-600' : ''} 
-                               ${color === 'yellow' ? 'text-yellow-600' : ''} 
-                               ${color === 'red' ? 'text-red-600' : ''} 
-                               ${color === 'pink' ? 'text-pink-600' : ''}`}
-          >
+            <Text className="text-2xl font-bold" style={{ color }}>
+              {bloodPressure.systolic}/{bloodPressure.diastolic}
+            </Text>
+          </View>
+          <Text className={`text-lg font-bold mt-4`} style={{ color }}>
             {status}
           </Text>
         </View>
-      </View>
-      {/* Input dan Gauge untuk Sistolik */}
-      <View className="w-full mb-6">
-        <Text className="text-base font-bold text-gray-700 mb-2">Sistolik</Text>
-        <View className="flex flex-row items-center space-x-2">
+
+        {/* Input Section */}
+        <View className="w-full mb-6">
+          <Text className="text-base font-bold text-gray-700 mb-2">
+            Sistolik
+          </Text>
           <TextInput
             placeholder="120"
             keyboardType="numeric"
             value={systolic}
             onChangeText={setSystolic}
-            className="flex-1 border border-gray-300 rounded-lg p-2 text-center"
+            className="border border-gray-300 rounded-lg p-2 text-center mb-4"
           />
-          <TouchableOpacity
-            onPress={saveBloodPressure}
-            className="bg-blue-500 py-2 px-4 rounded-lg flex flex-row items-center justify-center"
-          >
-            <FaSave className="text-white mr-2" />
-            <Text className="text-white font-bold">Save</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      {/* Input dan Gauge untuk Diastolik */}
-      <View className="w-full mb-6">
-        <Text className="text-base font-bold text-gray-700 mb-2">
-          Diastolik
-        </Text>
-        <View className="flex flex-row items-center space-x-2">
+          <Text className="text-base font-bold text-gray-700 mb-2">
+            Diastolik
+          </Text>
           <TextInput
             placeholder="80"
             keyboardType="numeric"
             value={diastolic}
             onChangeText={setDiastolic}
-            className="flex-1 border border-gray-300 rounded-lg p-2 text-center"
+            className="border border-gray-300 rounded-lg p-2 text-center mb-4"
           />
           <TouchableOpacity
             onPress={saveBloodPressure}
-            className="bg-blue-500 py-2 px-4 rounded-lg flex flex-row items-center justify-center"
+            className="bg-blue-500 py-2 px-4 rounded-lg"
           >
-            <FaSave className="text-white mr-2" />
-            <Text className="text-white font-bold">Save</Text>
+            <Text className="text-white font-bold text-center">Save</Text>
           </TouchableOpacity>
         </View>
-      </View>
 
-      {/* Keterangan Warna */}
-      <View className="w-full">
-        <Text className="text-base font-bold text-gray-700 mb-2">
-          Keterangan:
-        </Text>
-        <View className="flex flex-row items-center space-x-4">
-          <View className="flex items-center">
-            <View className="w-4 h-4 bg-green-600 rounded-full mb-1" />
-            <Text className="text-xs text-gray-600">Normal</Text>
-          </View>
-          <View className="flex items-center">
-            <View className="w-4 h-4 bg-yellow-500 rounded-full mb-1" />
-            <Text className="text-xs text-gray-600">Prahipertensi</Text>
-          </View>
-          <View className="flex items-center">
-            <View className="w-4 h-4 bg-red-600 rounded-full mb-1" />
-            <Text className="text-xs text-gray-600">Hipertensi 1</Text>
-          </View>
-          <View className="flex items-center">
-            <View className="w-4 h-4 bg-pink-600 rounded-full mb-1" />
-            <Text className="text-xs text-gray-600">Hipertensi 2</Text>
-          </View>
+        {/* Recommendations */}
+        <View className="w-full bg-white p-4 rounded-lg shadow-md">
+          <Text className="text-lg font-bold text-gray-700 mb-4">
+            Rekomendasi Tindakan
+          </Text>
+          {recommendations.tips && (
+            <View className="mb-4">
+              {recommendations.tips.map((tip, index) => (
+                <Text key={index} className="text-gray-600 mb-2">
+                  - {tip}
+                </Text>
+              ))}
+            </View>
+          )}
+
+          <Text className="text-lg font-bold text-gray-700 mb-4">
+            Rekomendasi Makanan
+          </Text>
+          {recommendations.foods &&
+            recommendations.foods.map((food, index) => (
+              <View key={index} className="mb-4">
+                <Text className="text-gray-600 font-bold">{food.type}:</Text>
+                {food.items.map((item, idx) => (
+                  <Text key={idx} className="text-gray-600">
+                    - {item}
+                  </Text>
+                ))}
+              </View>
+            ))}
+
+          <Text className="text-lg font-bold text-gray-700 mb-4">
+            Komplikasi yang Dapat Dihindari
+          </Text>
+          {recommendations.complications &&
+            recommendations.complications.map((complication, index) => (
+              <Text key={index} className="text-gray-600 mb-2">
+                - {complication}
+              </Text>
+            ))}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
