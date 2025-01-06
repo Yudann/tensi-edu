795d7e6a9b36e81c0f1c937d1abee03f2b9ffdcd
Yudann <yudannnnn80@gmail.com>
2025-01-06 21:39:08 +0700

    chore: update url path

--- app/(tabs)/articles.jsx
+++ app/(tabs)/articles.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
-
-const articles = [
-  { id: '1', title: 'Manfaat Olahraga' },
-  { id: '2', title: 'Pentingnya Pola Makan Sehat' },
-  { id: '3', title: 'Tips Hidup Seimbang' },
-];
+import articles from '../(screen)/articles/ArticlesData';
 
 export default function Articles() {
   const router = useRouter();
 
   return (
     <View className="flex-1 bg-white p-4">
-      {articles.map((article) => (
+      {Object.entries(articles).map(([id, article]) => (
         <TouchableOpacity
-          key={article.id}
+          key={id}
           className="w-full h-20 bg-blue-300 flex-row items-center justify-between px-6 rounded-xl mb-4"
-          onPress={() => router.push(`/article/${article.id}`)}
+          onPress={() => router.push(`/articles/${id}`)}
         >
           <Text className="font-bold text-lg">{article.title}</Text>
           <Text>→</Text>
